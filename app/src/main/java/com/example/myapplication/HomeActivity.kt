4ac69651db89253361897397b82a58af0bc3b784
Badhon3201 <badhon9265@gmail.com>
2023-08-08 18:26:46 +0600

    data save from share preference

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity : AppCompatActivity() {
+    var buttonLoginSignUp: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        buttonLoginSignUp = findViewById(R.id.loginSignUp)
+
+        buttonLoginSignUp!!.setOnClickListener {
+            val i = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.myapplication.adapter.OnBoardingViewPageAdapter
 import com.example.myapplication.model.OnBoardingData
@@ -17,10 +19,20 @@ class MainActivity : AppCompatActivity() {
     var onBoardingViewPager: ViewPager? = null
     var next: TextView? = null
     var position = 0
+    private var sharePreferences: SharedPreferences? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        if (restoredPrefData()) {
+            val i = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
         setContentView(R.layout.activity_main)
         tabLayout = findViewById(R.id.tabIndicator)
         next = findViewById(R.id.next)
@@ -58,7 +70,11 @@ class MainActivity : AppCompatActivity() {
                 position++
                 onBoardingViewPager!!.currentItem = position
 
-                Log.d("Data", "${onBoardingViewPager!!.currentItem}")
+            }
+            if (position == onBoardingData.size) {
+                savePrefData()
+                val i = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(i)
             }
         }
 
@@ -73,15 +89,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
 
         })
+    }
+
+    private fun savePrefData() {
+        sharePreferences = applicationContext.getSharedPreferences("Pref", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharePreferences!!.edit()
+        editor.putBoolean("isFirstTimeRun", true)
+        editor.apply()
+    }
+
+    private fun restoredPrefData(): Boolean {
+        sharePreferences = applicationContext.getSharedPreferences("Pref", Context.MODE_PRIVATE)
+        return sharePreferences!!.getBoolean("isFirstTimeRun", false)
     }
 
     private fun setOnBoardingViewPageAdapter(onBoardingData: List<OnBoardingData>) {
